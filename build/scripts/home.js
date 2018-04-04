ed7ab860290edc3a904f1065ecfa5d37800477a6
Michael Wallace <mtwallac@mtu.edu>
2018-04-03 21:07:40 -0400

    added modal support for email contact skeleton for future development of modal http://izimodal.marcelodolce.com/#modal-default

--- build/scripts/home.js
+++ build/scripts/home.js
@@ -4,11 +4,45 @@ window.onorientationchange = function() { document.body.scrollTop = 0; }
 
 $(document).ready(function(){
 
-  // open modal
-  $('.email').on('touch click', function(){
-    $('.emial').trigger('click');
+
+
+  $("#myModal").iziModal({
+    title: 'Contact Me',
+    headerColor: '#5A2B74',
+    background: null,
+    borderBottom: false,
+    theme: '',  // light
+    icon: null,
+    iconText: null,
+    iconColor: '',
+    rtl: false,
+    width: '60%',
+    top: '20vh',
+    radius: 7,
+    zindex: 999,
+    focusInput: true,
+    restoreDefaultContent: false,
+    openFullscreen: false,
+    closeOnEscape: true,
+    closeButton: true,
+    appendTo: 'body', // or false
+    appendToOverlay: 'body', // or false
+    overlay: true,
+    overlayClose: false,
+    overlayColor: 'rgba(0, 0, 0, 0.4)',
+    transitionIn: 'bounceInDown',
+    onFullscreen: function(){},
+    onResize: function(){},
+    onOpening: function(){},
+    onOpened: function(){},
+    onClosing: function(){},
+    onClosed: function(){},
+    afterRender: function(){}
   });
 
-  // close modal
-  $('a.close-reveal-modal').trigger('click');
+  // open modal
+  $('.email').on('touch click', function(){
+    $('#myModal').iziModal('open');
+  });
+
 });
